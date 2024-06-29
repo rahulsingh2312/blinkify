2e7ed0906676276f0d7940da5f3f255493acbf92
Rahul <rahulsinghhh2312@gmail.com>
2024-06-29 13:08:28 +0530

    wallet address

--- src/app/api/transfer/const.js
+++ src/app/api/transfer/const.js
@@ -1,7 +1,7 @@
 import { PublicKey } from "@solana/web3.js";
 
 export const DEFAULT_SOL_ADDRESS = new PublicKey(
-  "93bk5ApDHZ1fedg7FKsqtHUaqd9NvyCoijEEawuu1Cjm", // devnet wallet
+  "4iG4s2F3eSByCkMvfsGhrvzXNoPrDFUJuA7Crtuf3Pvn", // devnet wallet
 );
 
 export const DEFAULT_SOL_AMOUNT = 1.0;
